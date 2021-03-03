17343f71c55790596ce10cf47b0345cafbe7a123
z <alex@dwfe.ru>
2021-03-03 23:54:47 +0300

    .

--- packages/browsers-checks/src/routes.tsx
+++ packages/browsers-checks/src/routes.tsx
@@ -1,10 +1,10 @@
 import {IActionResult, IRoute} from '@do-while-for-each/path-resolver';
 import React, {ReactElement} from 'react'
-import {IRouteNote, NotFoundPage, TCtx} from './routing'
+import {IRouteNote, NotFoundPage} from './routing'
 import {EventLoopPage, IndexPage, RxJsPage} from './pages'
 
 
-export const routes: IRoute<ReactElement, IRouteNote, IActionResult<ReactElement>, TCtx>[] = [
+export const routes: IRoute<ReactElement, IRouteNote, IActionResult<ReactElement>>[] = [
   {path: '', component: <IndexPage/>},
   {path: 'event-loop', component: <EventLoopPage/>},
   {path: 'rxjs', component: <RxJsPage/>},
--- packages/browsers-checks/src/routing/contract.ts
+++ packages/browsers-checks/src/routing/contract.ts
@@ -2,14 +2,10 @@ import {IActionData, TRouteContext} from '@do-while-for-each/path-resolver'
 import {IResultListenersArg} from '@do-while-for-each/browser-router'
 import {HTMLProps, ReactElement} from 'react';
 
-export type TCtx = TRouteContext & { // this context for example
-  title?: string;
-} | null // because history package type 'State' = object | null
-
-export type TRouteActionData = IActionData<IRouteNote, TCtx>
+export type TRouteActionData = IActionData<IRouteNote>
 export type TRouteResultArg = IResultListenersArg<ReactElement, TRouteActionData>
 
-export type RouteActionData = IActionData<IRouteNote, TCtx>
+export type RouteActionData = IActionData<IRouteNote>
 
 export interface IRouteNote {
   title?: string;
