793cb7e9cb98189a7beaa8fed66b25824fa14c17
z <alex@dwfe.ru>
2021-03-13 23:24:34 +0300

    .

--- packages/browsers-checks/src/hooks/useControlledRender.ts
+++ packages/browsers-checks/src/hooks/useControlledRender.ts
@@ -2,5 +2,7 @@ import {useCallback, useState} from 'react'
 
 export const useControlledRender = () => { // const renderRunFn = useControlledRender()
   const [, updateState] = useState({})
-  return useCallback(() => updateState({}), [])
+  return useCallback(
+    () => updateState({})
+    , [])
 }
