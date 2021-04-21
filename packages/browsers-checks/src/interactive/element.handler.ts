dccd85a38166da9a0848b67fae51552679b4b74c
z <alex@dwfe.ru>
2021-04-21 22:53:04 +0300

    .

--- packages/browsers-checks/src/interactive/element.handler.ts
+++ packages/browsers-checks/src/interactive/element.handler.ts
@@ -1,4 +1,4 @@
-import {distinctUntilChanged, map, Observable, pairwise, Stopper, takeUntil, tap} from '@do-while-for-each/rxjs'
+import {distinctUntilChanged, map, Observable, pairwise, share, Stopper, takeUntil, tap} from '@do-while-for-each/rxjs'
 import {Point} from '@do-while-for-each/math'
 import {IElementHandleWrap, IMoveEvent, IUnpackedEvent} from './contract'
 import {DownEvent, MoveEvent, processMoveEvent, UpEvent} from './event'
@@ -17,21 +17,25 @@ export class ElementHandler {
               private wrap: IElementHandleWrap) {
     this.down$ = DownEvent.of$(element, wrap.rectHandler).pipe(
       takeUntil(this.stopper.ob$),
+      share(),
     )
     this.up$ = UpEvent.of$(element, wrap.rectHandler).pipe(
       takeUntil(this.stopper.ob$),
+      share(),
     )
     this.move$ = MoveEvent.of$(element, wrap.rectHandler).pipe(
       distinctUntilChanged((a, b) => Point.isEquals(a.pagePoint, b.pagePoint)),
       pairwise(),
       map(([a, b]) => processMoveEvent(a, b)),
       takeUntil(this.stopper.ob$),
+      share(),
     )
     this.drag$ = DragEvent.of$(this.down$, wrap.rectHandler, wrap.element).pipe(
       tap(drag => {
         drag.target = element
       }),
       takeUntil(this.stopper.ob$),
+      share(),
     )
   }
 
--- packages/browsers-checks/src/interactive/interactive.ts
+++ packages/browsers-checks/src/interactive/interactive.ts
@@ -1,4 +1,4 @@
-import {map, merge, Observable, scan, shareReplay} from '@do-while-for-each/rxjs'
+import {map, merge, Observable, scan, share} from '@do-while-for-each/rxjs'
 import {WebMatrix} from '@do-while-for-each/math'
 import {InteractiveType, ITransformData, ITransformGenerator} from './contract'
 import {ElementHandler} from './element.handler'
@@ -24,75 +24,13 @@ export class Interactive {
     this.elementHandler = new ElementHandler(element, {element: elementWrap, rectHandler: this.rectHandler})
     this.init()
 
-
-    // // const up$ = UpEvent.of$(element, this.rectHandler)
-    // const subscr1 = this.elementHandler.drag$.subscribe(()=> console.log(`drag-1`,))
-    // const subscr2 = this.elementHandler.drag$.subscribe(()=> console.log(`drag-2`,))
-    // // const subscr1 = drag$.subscribe(() => console.log(`drag-1`,))
-    // // const subscr2 = drag$.subscribe(() => console.log(`drag-2`,))
-    //
-    // setTimeout(() => {
-    //   subscr1.unsubscribe()
-    //   subscr2.unsubscribe()
-    //   console.log(`unsubscr 1, 2`,)
-    // }, 10_000)
-    // setTimeout(() => {
-    //   const subscr3 = this.elementHandler.drag$.subscribe(()=> console.log(`drag-3`,))
-    //   const subscr4 = this.elementHandler.drag$.subscribe(()=> console.log(`drag-4`,))
-    //   // const subscr3 = drag$.subscribe(() => console.log(`drag-3`,))
-    //   // const subscr4 = drag$.subscribe(() => console.log(`drag-4`,))
-    //   console.log(`subscr 3, 4`,)
-    // }, 20_000)
-
-
-    // // const up$ = UpEvent.of$(element, this.rectHandler)
-    // const subscr1 = this.elementHandler.up$.subscribe(()=> console.log(`up-1`,))
-    // const subscr2 = this.elementHandler.up$.subscribe(()=> console.log(`up-2`,))
-    // // const subscr1 = up$.subscribe(() => console.log(`up-1`,))
-    // // const subscr2 = up$.subscribe(() => console.log(`up-2`,))
-    //
-    // setTimeout(() => {
-    //   subscr1.unsubscribe()
-    //   subscr2.unsubscribe()
-    //   console.log(`unsubscr 1, 2`,)
-    // }, 10_000)
-    // setTimeout(() => {
-    //   const subscr3 = this.elementHandler.up$.subscribe(()=> console.log(`up-3`,))
-    //   const subscr4 = this.elementHandler.up$.subscribe(()=> console.log(`up-4`,))
-    //   // const subscr3 = up$.subscribe(() => console.log(`up-3`,))
-    //   // const subscr4 = up$.subscribe(() => console.log(`up-4`,))
-    //   console.log(`subscr 3, 4`,)
-    // }, 20_000)
-
-
-    // // const up$ = UpEvent.of$(element, this.rectHandler)
-    // const subscr1 = this.elementHandler.down$.subscribe(()=> console.log(`down-1`,))
-    // const subscr2 = this.elementHandler.down$.subscribe(()=> console.log(`down-2`,))
-    // // const subscr1 = down$.subscribe(() => console.log(`down-1`,))
-    // // const subscr2 = down$.subscribe(() => console.log(`down-2`,))
-    //
-    // setTimeout(() => {
-    //   subscr1.unsubscribe()
-    //   subscr2.unsubscribe()
-    //   console.log(`unsubscr 1, 2`,)
-    // }, 10_000)
-    // setTimeout(() => {
-    //   const subscr3 = this.elementHandler.down$.subscribe(()=> console.log(`down-3`,))
-    //   const subscr4 = this.elementHandler.down$.subscribe(()=> console.log(`down-4`,))
-    //   // const subscr3 = down$.subscribe(() => console.log(`down-3`,))
-    //   // const subscr4 = down$.subscribe(() => console.log(`down-4`,))
-    //   console.log(`subscr 3, 4`,)
-    // }, 20_000)
-
-
     this.transform$ = this.data$.pipe(
       map(action => action.matrix),
-      shareReplay(0),
+      share(),
     )
-    this.transformResult$ = this.data$.pipe(
-      map(action => action.matrix),
+    this.transformResult$ = this.transform$.pipe(
       scan((acc, curr) => acc.multiply(curr)),
-      shareReplay(0),
+      share(),
     )
   }
 
@@ -105,7 +43,7 @@ export class Interactive {
     this.data$ = merge(
       ...generators.map(generator => generator.data$)
     ).pipe(
-      shareReplay(0)
+      share()
     )
   }
 
