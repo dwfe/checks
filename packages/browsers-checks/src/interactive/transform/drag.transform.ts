2174cc54bb6f947498abcb5661333b0d8d634702
z <alex@dwfe.ru>
2021-07-25 20:57:35 +0300

    .

--- packages/browsers-checks/src/interactive/transform/drag.transform.ts
+++ packages/browsers-checks/src/interactive/transform/drag.transform.ts
@@ -1,11 +1,8 @@
 import {map, Observable} from '@do-while-for-each/rxjs'
-import {WebMatrix} from '@do-while-for-each/math'
 import {InteractiveVariant, ITransformData, ITransformGenerator} from '../contract'
 import {ElementHandler} from '../handler'
 
 export class DragTransform implements ITransformGenerator {
-
-  identityMatrix = WebMatrix.identity();
 
   constructor(private handler: ElementHandler) {
   }
@@ -13,7 +10,7 @@ export class DragTransform implements ITransformGenerator {
   data$: Observable<ITransformData> = this.handler.drag$.pipe(
     map(drag => ({
       variant: InteractiveVariant.DRAG,
-      matrix: WebMatrix.translateIdentity(drag.pagePointDiff[0], drag.pagePointDiff[1]),
+      matrix: [1, 0, 0, 1, drag.pagePointDiff[0], drag.pagePointDiff[1]],
       target: drag.target,
       event: drag.currEvent,
     })),
