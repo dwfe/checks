d0707fc7ef0544d728f78b0f8563cf316fed250a
z <alex@dwfe.ru>
2021-07-26 22:08:01 +0300

    .

--- packages/browsers-checks/src/interactive/event/mouse/mouse-wheel.event.ts
+++ packages/browsers-checks/src/interactive/event/mouse/mouse-wheel.event.ts
@@ -1,8 +1,10 @@
-import {fromEvent, Observable} from '@do-while-for-each/rxjs'
+import {fromEvent, Observable, tap} from '@do-while-for-each/rxjs'
 
 export class MouseWheel {
 
-  static event$ = (element: Element, options: AddEventListenerOptions = {}): Observable<WheelEvent> =>
-    fromEvent<WheelEvent>(element, 'wheel', options)
+  static event$ = (element: Element, options: AddEventListenerOptions = {passive: false}): Observable<WheelEvent> =>
+    fromEvent<WheelEvent>(element, 'wheel', options).pipe(
+      tap(e => e.preventDefault()), // при passive: false решает проблему скалинга страницы при Pinch Zoom(2мя пальцами), для iPad и Chrome/Mac
+    )
 
 }
--- packages/browsers-checks/src/interactive/transform/scale.transform.ts
+++ packages/browsers-checks/src/interactive/transform/scale.transform.ts
@@ -8,12 +8,24 @@ export class ScaleTransform implements ITransformGenerator {
   constructor(private handler: WrapHandler) {
   }
 
+  /**
+   * deltaMode - единица измерения длины прокручиваемых значений: 0 = pixels, 1 = lines, 2 = pages
+   *    deltaX - прокрутка влево/вправо
+   *    deltaY - вверх/вниз
+   *    deltaZ - вдоль оси Z
+   */
+  protected getScale(event: WheelEvent) {
+    let factor = 0.002;
+    if (event.deltaMode === 1)
+      factor *= 100 / 3;
+    return Math.pow(2, event.deltaY * factor * (event.shiftKey ? 0.1 : 1) * (event.ctrlKey ? 4 : 1));
+  }
+
   data$: Observable<ITransformData> = this.handler.wheel$.pipe(
     withLatestFrom(this.handler.cursorPos$),
     map(([wheel, cursorPos]) => {
-      const factor = wheel.deltaY < 0 ? 1.1 : 0.9;
       const result: ITransformData = {
-        matrix: WebMatrix.scaleAtPoint(cursorPos.pagePoint, factor)
+        matrix: WebMatrix.scaleAtPoint(cursorPos.pagePoint, this.getScale(wheel))
       }
       if (cursorPos.extra)
         result.extra = {
