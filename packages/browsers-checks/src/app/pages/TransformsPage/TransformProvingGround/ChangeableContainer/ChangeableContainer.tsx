c422c26c3df12c6e9fba3c233bb2c20e38580f8d
z <alex@dwfe.ru>
2021-07-27 23:46:30 +0300

    .

--- packages/browsers-checks/src/app/pages/TransformsPage/TransformProvingGround/ChangeableContainer/ChangeableContainer.tsx
+++ packages/browsers-checks/src/app/pages/TransformsPage/TransformProvingGround/ChangeableContainer/ChangeableContainer.tsx
@@ -1,23 +1,34 @@
-import {animationFrame, delay, startWith, tap} from '@do-while-for-each/rxjs'
+import {animationFrame, debounceTime, delay, startWith, tap} from '@do-while-for-each/rxjs'
+import React, {useEffect, useRef, useState} from 'react'
 import {WebMatrix} from '@do-while-for-each/math'
-import React, {useEffect, useRef} from 'react'
 import {ElementHandler, Interactive, WrapHandler} from '../../../../../interactive'
 import './ChangeableContainer.css'
 
+const inactiveText = 'Transform Me'
+
 export function ChangeableContainer({elementWrap}: IProps) {
   const ref = useRef<HTMLDivElement>(null)
+  const [text, setText] = useState(inactiveText)
 
   useEffect(() => {
+    const element = ref.current as HTMLDivElement;
+
     const wrapHandler = new WrapHandler(elementWrap)
-    const elementHandler = new ElementHandler(ref.current as HTMLDivElement, wrapHandler)
+    const elementHandler = new ElementHandler(element, wrapHandler)
     const interactive = new Interactive(wrapHandler, elementHandler)
+
     interactive.resultMatrix$.pipe(
       startWith(WebMatrix.identity()),
       delay(0, animationFrame),
-      tap(m => {
-        (ref.current as HTMLDivElement).style.transform = WebMatrix.toStyleValue(m)
-      })
+      tap(m => element.style.transform = WebMatrix.toStyleValue(m))
     ).subscribe()
+
+    interactive.resultMatrix$.pipe(
+      tap(() => setText('Yeah!')),
+      debounceTime(500),
+      tap(() => setText(inactiveText)),
+    ).subscribe()
+
     return () => {
       wrapHandler.stop()
       elementHandler.stop()
@@ -26,7 +37,7 @@ export function ChangeableContainer({elementWrap}: IProps) {
 
   return (
     <div className="ChangeableContainer" ref={ref}>
-
+      <div>{text}</div>
     </div>
   );
 }
