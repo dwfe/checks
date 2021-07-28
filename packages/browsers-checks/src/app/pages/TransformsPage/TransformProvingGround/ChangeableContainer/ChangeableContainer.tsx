66f11466ade43b23a993510ffc2bab3bef9b860e
z <alex@dwfe.ru>
2021-07-28 12:25:43 +0300

    .

--- packages/browsers-checks/src/app/pages/TransformsPage/TransformProvingGround/ChangeableContainer/ChangeableContainer.tsx
+++ packages/browsers-checks/src/app/pages/TransformsPage/TransformProvingGround/ChangeableContainer/ChangeableContainer.tsx
@@ -1,6 +1,6 @@
 import {animationFrame, debounceTime, delay, startWith, tap} from '@do-while-for-each/rxjs'
-import {AngleType, WebMatrix} from '@do-while-for-each/math'
 import React, {useEffect, useRef, useState} from 'react'
+import {WebMatrix} from '@do-while-for-each/math'
 import {ElementHandler, ElementInteractive, WrapHandler} from '../../../../../interactive'
 import './ChangeableContainer.css'
 
@@ -16,7 +16,7 @@ export function ChangeableContainer({wrapHandler}: IProps) {
     const handler = new ElementHandler(element, wrapHandler)
     const interactive = new ElementInteractive(
       handler,
-      WebMatrix.of().translate(50, 120).rotate(-10, AngleType.DEGREES).toJSON()
+      WebMatrix.of().translate(50, 120).rotate(-10).toJSON()
     )
 
     interactive.resultMatrix$.pipe(
