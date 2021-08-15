6f37f2f1ff52119fbd5fade1ce21a91eeb7a7721
z <alex@dwfe.ru>
2021-08-15 22:16:28 +0300

    .

--- packages/browsers-checks/src/app/page/CanvasPage/IsPointIn/IsPointIn.tsx
+++ packages/browsers-checks/src/app/page/CanvasPage/IsPointIn/IsPointIn.tsx
@@ -87,25 +87,25 @@ export function IsPointIn() {
   )
 }
 
-function drawGrid(ctx: CanvasRenderingContext2D, xGrid: IGridData, yGrid: IGridData) {
+function drawGrid(ctx: CanvasRenderingContext2D, xParams: IGridData, yParams: IGridData) {
   ctx.lineWidth = 1
   ctx.strokeStyle = 'rgba(255,255,0,0.5)'
 
   //x
-  for (let i = xGrid.from; i <= xGrid.to; i += xGrid.step) {
+  for (let i = xParams.from; i <= xParams.to; i += xParams.step) {
     const pos = truncTo05(i)
     ctx.beginPath();
     ctx.moveTo(pos, 0);
-    ctx.lineTo(pos, yGrid.to);
+    ctx.lineTo(pos, yParams.to);
     ctx.stroke()
   }
 
   //y
-  for (let i = yGrid.from; i <= yGrid.to; i += yGrid.step) {
+  for (let i = yParams.from; i <= yParams.to; i += yParams.step) {
     const pos = truncTo05(i)
     ctx.beginPath();
     ctx.moveTo(0, pos);
-    ctx.lineTo(xGrid.to, pos);
+    ctx.lineTo(xParams.to, pos);
     ctx.stroke()
   }
 }
